3ca6afe3b84fdc60b7341ce26e01da14f43547f1
garylvov <gary.lvov@gmail.com>
2025-01-12 06:33:22 -0500

    precommit

--- therapeutech/develop.sh
+++ therapeutech/develop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 xhost +local: || true  && \
- python3 docker.py tt_ubuntu22_python310:latest -i -v therapeutech 
+ python3 docker.py tt_ubuntu22_python310:latest -i -v therapeutech
