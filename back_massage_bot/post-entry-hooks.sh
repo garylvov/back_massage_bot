7c3d828d283f511a460120579e7d9a7b2c211252
garylvov <gary.lvov@gmail.com>
2025-01-13 00:24:29 -0500

    cache builds better

--- back_massage_bot/post-entry-hooks.sh
+++ back_massage_bot/post-entry-hooks.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-
+# If you modify me, your next docker build may take long!
 # Check if we're in the right environment, if not activate it
 if [[ "$CONDA_DEFAULT_ENV" != "back_massage_bot" ]]; then
     conda activate back_massage_bot
 fi
-# Install packages
+# Install packages (if they haven't been)
 mim install mmengine && \
 mim install "mmcv>=2.0.1" && \
 mim install "mmdet>=3.1.0" && \
-echo "Activated Conda, ran Mim Install."
+echo "Activated Conda, installed or checked MMPose."
