220a140193def672cef087d2dcb388ca0063f66d
garylvov <gary.lvov@gmail.com>
2025-01-14 18:35:19 -0500

    remove submodule

--- ws/src/main_ros/develop.sh
+++ ws/src/main_ros/develop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 xhost +local: || true  && \
- python3 docker.py -c bmb_ubuntu22_humble:latest -i -v back_massage_bot,ws/src/main_ros
+ python3 docker.py -c bmb_ubuntu22_humble:latest -i -v back_massage_bot,ws/src/main_ros --devices "/dev/video0,/dev/video1,/dev/video2,/dev/video3,/dev/video4,/dev/video6,/dev/video7"
