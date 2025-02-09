2d3aaea75e65c5392ecc44f4daa59c1fc5e39bba
garylvov <gary.lvov@gmail.com>
2025-02-09 14:58:27 -0500

    update entrypoint

--- ws/src/main_ros/entrypoint.sh
+++ ws/src/main_ros/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-source post-entry-hooks.sh && \
-ros2 launch back_massage_bot_ros camera-bringup.py > realsense_log.txt & \
-ros2 launch kinova_bringup kinova_robot_launch.py > kinova_log.txt & \
-ros2 launch kinova_bringup moveit_robot_launch.py > moveit_log.txt & \
-ros2 run kinova_driver joint_trajectory_action_server j2n6s300 > joint_trajectory_action_server_log.txt & \
-ros2 run kinova_driver gripper_command_action_server j2n6s300 > gripper_command_action_server_log.txt & 
+
+# Source hooks
+source post-entry-hooks.sh
+
+# Launch processes in background and capture logs
+ros2 launch back_massage_bot_ros camera-bringup.py > realsense_log.txt &
+ros2 launch kinova_bringup kinova_robot_launch.py use_jaco_v1_fingers:=false > kinova_log.txt &
+ros2 run kinova_driver joint_trajectory_action_server j2n6s300 > joint_trajectory_action_server_log.txt &
+ros2 run kinova_driver gripper_command_action_server j2n6s300 > gripper_command_action_server_log.txt &
+ros2 launch kinova_bringup moveit_robot_launch.py > moveit_log.txt
