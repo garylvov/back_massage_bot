4c4beca25ec3d257f90ce21f365dfb614fb33239
garylvov <gary.lvov@gmail.com>
2025-03-13 00:44:35 -0400

    fix post entry

--- ws/src/main_ros/post-entry-hooks.sh
+++ ws/src/main_ros/post-entry-hooks.sh
@@ -2,16 +2,19 @@
 pushd . # Save current directory
 source /back_massage_bot/post-entry-hooks.sh && \
 cd /ws && \
-colcon build --symlink-install \
---cmake-args \
--DCMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu \
--DPython3_EXECUTABLE=$HOME/miniforge3/envs/back_massage_bot/bin/python3.1 1\
-colcon build --packages-select \
+colcon build \
+--symlink-install \
+--parallel-workers $(nproc) \
+--packages-select \
 back_massage_bot_ros \
 synchros2 \
 kinova_driver \
-kinova_bringup kinova_msgs \
+kinova_bringup \
+kinova_msgs \
 kinova_description \
-kinova_demo
+kinova_demo \
+--cmake-args \
+-DCMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu \
+-DPython3_EXECUTABLE=$HOME/miniforge3/envs/back_massage_bot/bin/python3.11
 source /ws/install/setup.bash || true
 popd # Return to original directory
