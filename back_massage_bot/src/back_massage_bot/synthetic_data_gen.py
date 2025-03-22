228826230d6ac1929a6e1a75ca9fa32adc7f7fa5
garylvov <gary.lvov@gmail.com>
2025-03-22 16:58:14 -0400

    create fol

--- back_massage_bot/src/back_massage_bot/synthetic_data_gen.py
+++ back_massage_bot/src/back_massage_bot/synthetic_data_gen.py
@@ -6,6 +6,8 @@
 import numpy as np
 
 
+bbox_names = ['back', 'head', 'right_arm', 'left_arm', 'right_leg', 'left_leg', 'glutes']
+
 def generate_synthetic_top_down_massage_occupancy_grid(image_size: tuple[int, int], 
                     grid_resolution: float,
                     head_width_bounds: tuple[float, float],
@@ -30,13 +32,13 @@ def generate_synthetic_top_down_massage_occupancy_grid(image_size: tuple[int, in
                     populated_cell_single_decimation_prob: float,
                     empty_cell_single_addition_prob: float,
                     empty_cell_cluster_addition_prob: float,
-                    
-                    debug_viz = False):
+                    empty_cell_cluster_addition_num_pixel_bounds: tuple[int, int],
+                    debug_viz = False) -> tuple[np.ndarray, list[dict]]:
     """Generate top down synthetic occupancy grid of the human body for a massage bot. Bake in some noise from the jump.
     This could be done as part of an augmentation step in the training pipeline, but for the sake of convenience, we are doing it here."""
     image = np.zeros(image_size, dtype=np.uint8)
 
-
+def write_synthetic_data_to_yolov11_file(image: np.ndarray, bboxes: list[dict], output_file_path: str):
 
 if __name__ == "__main__":
     generate_synthetic_top_down_massage_occupancy_grid(debug_viz=True)
